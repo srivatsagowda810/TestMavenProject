532f0310bf02eee8ecbfc583649ec0631caf20d4
Ike731 <aaibaidila@gmail.com>
2019-12-26 22:36:22 -0500

    Adding Calculator to the application

---
+++ src/main/java/cybertek/Calculator.java
@@ -0,0 +1,54 @@
+package cybertek;
+
+/**
+ * @author : Akbar
+ * Created At : 12/26/19
+ */
+public class Calculator {
+
+    /**
+     * Add methods to add 2 numbers
+     *
+     * @param d1 first number
+     * @param d2 second number
+     * @return calculated sum of two numbers
+     */
+    public static double add(double d1, double d2) {
+        return d1 + d2;
+    }
+
+    /**
+     * subtract methods to subtract 2 numbers
+     *
+     * @param d1 first number
+     * @param d2 second number
+     * @return calculated subtraction of two numbers
+     */
+    public static double subtract(double d1, double d2) {
+        return d1 - d2;
+    }
+
+    /**
+     * multiply methods to multiply 2 numbers
+     *
+     * @param d1 first number
+     * @param d2 second number
+     * @return calculated multiplication of two numbers
+     */
+    public static double multiply(double d1, double d2) {
+        return d1 * d2;
+    }
+
+    /**
+     * divide methods to add 2 numbers
+     *
+     * @param d1 first number
+     * @param d2 second number
+     * @return calculated division of two numbers
+     */
+    public static double divide(double d1, double d2) {
+        return d1 / d2;
+    }
+
+
+}
---
+++ src/test/java/cybertek/CalculatorTest.java
@@ -0,0 +1,38 @@
+package cybertek;
+
+import org.junit.jupiter.api.Test;
+
+import static cybertek.Calculator.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class CalculatorTest {
+
+    @Test
+    void addTest() {
+        assertEquals(12.0, add(10, 2));
+    }
+
+    @Test
+    void subtractTest() {
+        assertEquals(5, subtract(10, 5));
+    }
+
+    @Test
+    void multiplyTest() {
+        assertEquals(50, multiply(10, 5));
+    }
+
+    @Test
+    void divideTest() {
+        assertEquals(2, divide(10, 5));
+    }
+
+    @Test
+    void divideTest2() {
+        assertThrows(ArithmeticException.class,
+                () -> System.out.println(10 / 0));
+    }
+
+
+}
